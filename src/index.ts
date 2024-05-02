7518f16b98b07cc611544abdf8d37e878bc2168b
Morgan Navel <navelmorgan34@gmail.com>
2024-05-02 18:25:57 +0200

    Add outils de debug

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { initDb } from "./sequelize";
 import { loadRoutes } from "./helper";
+import morgan from "morgan";
 dotenv.config();
 
 const port = process.env.API_PORT || 8080;
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 initDb();
 
 
